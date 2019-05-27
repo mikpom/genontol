96bf47bd249de1e1a9ed539ec4646da42347966c
Mikhail <pom@mailbox.org>
2019-05-27 11:55:55 -0700

    Fix enrich.py

--- genontol/enrich.py
+++ genontol/enrich.py
@@ -1,4 +1,5 @@
 # Copyright 2015-2019 Jan Daniel Rudolph (@jdrudolph)
+# Modified version Copyright 2019 Mikhail Pomaznoy (@mikpom)
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -17,146 +18,29 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-import random
+
 import networkx as nx
 import numpy as np
+import pandas as pd
 from scipy.stats import hypergeom
-from goenrich.tools import fdrcorrection
+from genontol.tools import fdrcorrection
 
-import goenrich.export
-
-def analyze(O, query, background_attribute, **kwargs):
-    """ run enrichment analysis for query
-
-    >>> O = goenrich.obo.ontology('db/go-basic.obo')
-    >>> gene2go = goenrich.read.gene2go('db/gene2go.gz')
-    >>> values = {k: set(v) for k,v in gene2go.groupby('GO_ID')['GeneID']}
-    >>> goenrich.enrich.propagate(O, values, 'gene2go')
-    >>> df = goenrich.enrich.analyze(O, query, ...)
-
-    :param O: Ontology graph after backgroud was set
-    :param query: array like of ids
-    :returns: pandas.DataFrame with results
-    """
-    options = {
-            'show' : 'top20',
-            'method' : 'benjamini-hochberg'
-    }
+def analyze(O, query, background_attribute, M=None, **kwargs):
+    options = {'show' : 'top20'}
     options.update(kwargs)
     _query = set(query)
-    terms, nodes = zip(*O.nodes(data=True))
-    all_ids = {x for n in nodes for x in n[background_attribute]} 
-    M = len(all_ids) # all ids used
+    terms, nodes = zip(*G.nodes(data=True))
+    if not M:
+        M = len({x for n in nodes for x in n[background_attribute]}) # all ids used
     N = len(_query)
     ps, xs, ns = calculate_pvalues(nodes, _query, background_attribute,
             M, **options)
-    if options['method'] == 'permutation':
-        qs, rejs = permuation_based_fdr(ps, N, all_ids, nodes, background_attribute, M, **options)
-    else:
-        qs, rejs = multiple_testing_correction(ps, **options)
-    df = goenrich.export.to_frame(nodes, term=terms, q=qs, rejected=rejs,
-            p=ps, x=xs, n=ns, M=M, N=N)
-    if 'gvfile' in options:
-        show = options['show']
-        if show.startswith('top'):
-            top = int(show.replace('top', ''))
-            sig = df.sort_values('q').head(top)['term']
-        else:
-            raise NotImplementedError(show)
-        G = induced_subgraph(O, sig)
-        for term, node, q, x, n, rej in zip(terms, nodes, qs, xs, ns, rejs):
-            if term in G:
-                G.node[term].update({'name' : node['name'], 'x' : x,
-                    'q' : q, 'n' : n, 'significant' : rej})
-        goenrich.export.to_graphviz(G.reverse(copy=False), **options)
+    qs, rejs = multiple_testing_correction(ps, **options)
+    names, namespaces = zip(*[(n['name'], n['namespace']) for n in nodes])
+    df = pd.DataFrame({'name':names, 'namespace':namespaces,
+                       'term':terms, 'q':qs, 'rejected':rejs,
+                       'p':ps, 'x':xs, 'n':ns, 'M':M, 'N':N})
     return df
-
-def permuation_based_fdr(ps, N, all_ids, nodes, background_attribute, M,
-        permutations = 1000, alpha = 0.05, seed=42, **options):
-    """ Calculate a permuation based FDR
-
-    >>> goenrich.enrich.analyze(..., method='permuation', permutations=1000)
-
-    :param ps: p-values of the query
-    :param N: number of elements in the query
-    :param all_ids: all identifiers occuring in the background
-    :param nodes: nodes in the ontology
-    :param background_attribute: name of the background_attribute
-    :param M: total number of terms
-    :param permutations: number of permutations
-    :param alpha: test significance level
-    :param seed: seed for the random number generator. keep fixed for reproducible results
-    """
-    # TODO NaN o-values should be ignored in the analysis
-    # create random p-values
-    _ps_random = []
-    random.seed(seed)
-    with np.errstate(invalid='ignore'):
-        for i in range(0, permutations):
-            random_query = set(random.sample(all_ids, N))
-            _ps, _, _ = calculate_pvalues(nodes, random_query, background_attribute,
-                    M, **options)
-            _ps_random.append(_ps)
-    ps_random = np.concatenate(_ps_random)
-    indices_random = np.full_like(ps_random, -1, dtype=int)
-    # combine random and real p-values
-    ps_all = np.append(ps_random, ps)
-    indices_all = np.append(indices_random, np.arange(0, len(ps)))
-    order = np.argsort(ps_all)[::-1]
-    indices = indices_all[order]
-    # iterate over sorted p-values to deterime qs
-    tp = 0
-    fp = 0
-    qs = np.zeros_like(ps)
-    for i in indices:
-        if i < 0:
-            fp = fp + 1
-        else:
-            tp = tp + 1
-            qs[i] = min(1, fp/tp/permutations)
-    rej = qs < alpha
-    return qs, rej
-
-def propagate(O, values, attribute):
-    """ Propagate values trough the hierarchy
-
-    >>> O = goenrich.obo.ontology('db/go-basic.obo')
-    >>> gene2go = goenrich.read.gene2go('db/gene2go.gz')
-    >>> values = {k: set(v) for k,v in gene2go.groupby('GO_ID')['GeneID']}
-    >>> goenrich.enrich.propagate(O, values, 'gene2go')
-
-    Uses topological sorting of the vertices. Since degrees are
-    usually low performance is almost linear time.
-
-    :param O: ontology graph
-    :param values: mapping of nodes to set of ids
-    :param attribute: name of the attribute
-    """
-    for n in nx.topological_sort(O):
-        current = O.node[n].setdefault(attribute, set())
-        current.update(values.get(n, set()))
-        for p in O[n]:
-            O.node[p].setdefault(attribute, set()).update(current)
-
-def induced_subgraph(O, terms):
-    """  Extracts a subgraph from O including the provided terms
-    and all higher hierarchy
-
-    >>> df = goenrich.enrich.analyze(O, ...)
-    >>> G = goenrich.induced_subgraph(O, df[df.rejected]['terms'])
-
-    :param O: ontology graph
-    :param terms: a list of terms to extract
-    """
-    roots = O.graph['roots'].values()
-    nodes = set()
-    for term in terms:
-        if term in nodes:
-            continue
-        for root in roots:
-            for path in nx.all_simple_paths(O, term, root):
-                nodes.update(path)
-    return O.subgraph(nodes)
 
 def calculate_pvalues(nodes, query, background_attribute, M,
         min_category_size=3, max_category_size=500,
--- genontol/obo.py
+++ genontol/obo.py
@@ -17,8 +17,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
 import itertools
 import networkx as nx
+import gzip
 
 def _tokenize(f):
     token = []
