a5102ca860bf17ef104ada5a3e8c487e5f2ce7c5
Mikhail <pom@mailbox.org>
2019-05-27 12:34:30 -0700

    Fix read.py

--- genontol/read.py
+++ genontol/read.py
@@ -35,13 +35,14 @@ def goa(filename, experimental=True, **kwds):
     :param experimental: use only experimentally validated annotations
     """
     defaults = {'comment' : '!',
-            'names': GENE_ASSOCIATION_COLUMNS}
+                'names': GENE_ASSOCIATION_COLUMNS,
+                'low_memory':False}
 
     if experimental and 'usecols' in kwds:
         kwds['usecols'] += ('evidence_code', )
 
     defaults.update(kwds)
-    result = pd.read_csv(filename, sep='\t', **defaults)
+    result = pd.read_table(filename, **defaults)
 
     if experimental:
         retain_mask = result.evidence_code.isin(EXPERIMENTAL_EVIDENCE)
@@ -66,9 +67,10 @@ def gene2go(filename, experimental=False, tax_id=9606, **kwds):
     :param tax_id: filter according to taxon
     """
     defaults = {'comment': '#',
-                'names': GENE2GO_COLUMNS}
+                'names': GENE2GO_COLUMNS,
+                'low_memory' : False}
     defaults.update(kwds)
-    result = pd.read_csv(filename, sep='\t', **defaults)
+    result = pd.read_table(filename, **defaults)
     
     retain_mask = result.tax_id == tax_id
     result.drop(result.index[~retain_mask], inplace=True)
